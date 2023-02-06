665a48855bc5a6f4a416b664f9dd5e6d0347cc27
ultyyp <yehia.super@gmail.com>
2023-02-06 18:22:47 +0200

    Add project files.

---
+++ ProglibIO/Functions.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+using static System.Net.WebRequestMethods;
+
+namespace ProglibIO
+{
+    public class Vacancy
+    {
+        public string VacancyName { get; set; }
+        public string VacancyURL { get; set; }
+        public string PublishingDate { get; set; }
+
+    }
+
+    public class Functions
+    {
+        public static Vacancy NewVacancy()
+        {
+            return new Vacancy();
+        }
+
+        public static async Task<List<string>> GetVacancyNamesAsync(string url)
+        {
+            List<string> finalResult = new List<string>();
+            var task = Task.Run(async () => {
+
+                IConfiguration config = Configuration.Default.WithDefaultLoader();
+                IBrowsingContext context = BrowsingContext.New(config);
+                AngleSharp.Dom.IDocument document = await context.OpenAsync(url);
+
+
+                string selector = "article > div > div > a > div.flex.align-between > h2";
+
+                AngleSharp.Dom.IHtmlCollection<AngleSharp.Dom.IElement> elements =
+                document.QuerySelectorAll(selector);
+
+                IEnumerable<string> results = elements.Select(it => it.TextContent);
+                finalResult = results.ToList();
+
+            });
+
+            await task;
+            return finalResult;
+        }
+
+        public static async Task<List<string>> GetVacancyURLSAsync(string url)
+        {
+            List<string> finalResult = new List<string>();
+            var task = Task.Run(async () => {
+
+                IConfiguration config = Configuration.Default.WithDefaultLoader();
+                IBrowsingContext context = BrowsingContext.New(config);
+                AngleSharp.Dom.IDocument document = await context.OpenAsync(url);
+
+
+                string selector = "article > div > div > a";
+
+                AngleSharp.Dom.IHtmlCollection<AngleSharp.Dom.IElement> elements =
+                document.QuerySelectorAll(selector);
+
+                IEnumerable<string> results = elements.Select(it => "https://proglib.io" + it.GetAttribute("href"));
+                finalResult = results.ToList();
+
+            });
+
+            await task;
+            return finalResult;
+        }
+
+        public static async Task<List<string>> GetPublishingDatesAsync(string url)
+        {
+            List<string> finalResult = new List<string>();
+            var task = Task.Run(async () => {
+
+                IConfiguration config = Configuration.Default.WithDefaultLoader();
+                IBrowsingContext context = BrowsingContext.New(config);
+                AngleSharp.Dom.IDocument document = await context.OpenAsync(url);
+
+
+                string selector = "article > header > div > div.preview-card__publish > div.publish-info";
+
+                AngleSharp.Dom.IHtmlCollection<AngleSharp.Dom.IElement> elements =
+                document.QuerySelectorAll(selector);
+
+                IEnumerable<string> results = elements.Select(it => it.TextContent);
+                finalResult = results.ToList();
+
+            });
+
+            await task;
+            return finalResult;
+        }
+
+        public async static Task<int> GetTotalPagesAsync()
+        {
+            int totalPages = 0;
+            string url = "https://proglib.io/vacancies/all?workType=all&workPlace=all&experience=&salaryFrom=&page=1";
+
+            IConfiguration config = Configuration.Default.WithDefaultLoader();
+            IBrowsingContext context = BrowsingContext.New(config);
+            AngleSharp.Dom.IDocument document = await context.OpenAsync(url);
+
+            string selector = "body > div.basis.sheet > div.basis__h-wrapper.sheet__center > div.basis__h-content > div > div > main > div.feed-pagination.flex.align-center";
+            var element = document.QuerySelector(selector);
+            totalPages = int.Parse(element.GetAttribute("data-total"));
+
+            return totalPages;
+
+        }
+
+        public static async Task<ConcurrentBag<Vacancy>> GetVacanciesByURLAsync(string url)
+        {
+            ConcurrentBag<Vacancy> vacancies = new ConcurrentBag<Vacancy>();
+
+            var names = await ProglibIO.Functions.GetVacancyNamesAsync(url);
+            var urls = await ProglibIO.Functions.GetVacancyURLSAsync(url);
+            var dates = await ProglibIO.Functions.GetPublishingDatesAsync(url);
+
+            var task = Task.Run(() => {
+                for (int i = 0; i < names.Count; i++)
+                {
+                    var vacancy = ProglibIO.Functions.NewVacancy();
+                    vacancy.VacancyName = names[i];
+                    vacancy.VacancyURL = urls[i];
+                    vacancy.PublishingDate = dates[i];
+
+                    vacancies.Add(vacancy);
+                };
+
+            });
+
+            await task;
+            return vacancies;
+        }
+
+        public static async Task<ConcurrentBag<Vacancy>> GetVacanciesFromAllPagesAsync()
+        {
+            int totalPages = await ProglibIO.Functions.GetTotalPagesAsync();
+            List<string> urls = new List<string>();
+            ConcurrentBag<Vacancy> finalBag = new ConcurrentBag<Vacancy>();
+
+            await Task.Run(() => {
+                for (int i = 1; i <= totalPages; i++)
+                {
+                    urls.Add("https://proglib.io/vacancies/all?workType=all&workPlace=all&experience=&salaryFrom=&page=" + i.ToString());
+                }
+            });
+
+            var options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = 50;
+            var token = options.CancellationToken;
+
+            await Parallel.ForEachAsync(urls, options, async (currentUrl, token) =>
+            {
+                ConcurrentBag<Vacancy> vacancies = await ProglibIO.Functions.GetVacanciesByURLAsync(currentUrl);
+                Parallel.ForEach(vacancies, vac => {
+                    finalBag.Add(vac);
+                });
+            });
+
+            return finalBag;
+        }
+
+
+
+
+    }
+}
---
+++ Website_Parser/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AngleSharp.Dom;
+using ProglibIO;
+
+namespace Website_Parser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StatusLabel.Content = "Extracting...";
+            Stopwatch sw = Stopwatch.StartNew();
+            ConcurrentBag<Vacancy> concBag = await ProglibIO.Functions.GetVacanciesFromAllPagesAsync();
+
+            foreach (var vac in concBag)
+            {
+                VacancyGrid.Items.Add(vac);
+            }
+
+            sw.Stop();
+            StatusLabel.Content = "Done! Elapsed: " + sw.Elapsed.ToString();
+            AmmountLabel.Content = "Vacancies: " + VacancyGrid.Items.Count.ToString();
+        }
+
+        private void VacancyGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var vacancy = (Vacancy)VacancyGrid.SelectedItem;
+            OpenUrl(vacancy.VacancyURL);
+        }
+
+
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
